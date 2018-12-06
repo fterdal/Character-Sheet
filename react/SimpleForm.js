2fbb7d8f4df552a55aff93f92da583afc96c0838
Finn Terdal <fterdal@gmail.com>
2018-12-06 17:03:59 -0600

    adds some base64 encoding

--- react/SimpleForm.js
+++ react/SimpleForm.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
+import { Base64 } from 'js-base64'
 
 class SomeForm extends React.Component {
   state = { addressBar: '' }
@@ -18,6 +19,8 @@ class SomeForm extends React.Component {
     this.setState({ addressBar: this.props.history.location.pathname.slice(1) })
   }
   render() {
+    const encodedAddressBar = Base64.encodeURI(this.state.addressBar)
+    console.log('encodedAddressBar: ', encodedAddressBar)
     return (
       <form onSubmit={this.handleSubmit}>
         <input name="addressBar" value={this.state.addressBar} onChange={this.handleChange} type="text" />
