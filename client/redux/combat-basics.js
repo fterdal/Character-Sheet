24e898b30edd4b5f067ba94d5e9168c3322ce453
Finn <finnterdal@gmail.com>
2019-06-04 11:37:55 -0500

    add hit dice to combatbasics reducer

--- client/redux/combat-basics.js
+++ client/redux/combat-basics.js
@@ -1,9 +1,13 @@
+// NOTE: Eventually, we'll calculate each of these stats from ability
+// modifiers and the user will be able to customize with misc modifiers.
+
 // ACTION TYPES
 const SET_PROFICIENCY = 'SET_PROFICIENCY'
 const SET_SPEED = 'SET_SPEED'
 const SET_MAX_HP = 'SET_MAX_HP'
 const SET_CURRENT_HP = 'SET_CURRENT_HP'
 const SET_ARMOR_CLASS = 'SET_ARMOR_CLASS'
+const SET_HIT_DICE = 'SET_HIT_DICE'
 
 // ACTION CREATORS
 export const setProf = prof => ({ type: SET_PROFICIENCY, prof })
@@ -14,6 +18,14 @@ export const setArmorClass = armorClass => ({
   type: SET_ARMOR_CLASS,
   armorClass,
 })
+/**
+ * hit dice is an object like this:
+ * { d6: 3, d10: 1 } for a character with 3d6+1d10 hit dice
+ */
+export const setHitDice = hitDice => ({
+  type: SET_ARMOR_CLASS,
+  hitDice,
+})
 
 const defaultState = {
   prof: 0,
@@ -21,6 +33,7 @@ const defaultState = {
   maxHP: 0,
   currentHP: 0,
   armorClass: 10,
+  hitDice: {},
 }
 
 const dispatchers = {
@@ -29,6 +42,7 @@ const dispatchers = {
   [SET_MAX_HP]: (state, { maxHP }) => ({ ...state, maxHP }),
   [SET_CURRENT_HP]: (state, { currentHP }) => ({ ...state, currentHP }),
   [SET_ARMOR_CLASS]: (state, { armorClass }) => ({ ...state, armorClass }),
+  [SET_HIT_DICE]: (state, { hitDice }) => ({ ...state, hitDice }),
 }
 
 export const combatBasicsReducer = (state = defaultState, action) => {
