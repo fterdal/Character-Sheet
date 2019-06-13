0a4d4ace3eda6081216cc24acb5d7e038f948bd3
Finn <finnterdal@gmail.com>
2019-06-13 15:24:58 -0500

    can toggle save proficiency, but still a little messy

--- client/components/AbilityScore.js
+++ client/components/AbilityScore.js
@@ -1,6 +1,15 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { setStr, setDex, setCon, setInt, setWis, setCha } from '../redux'
+import {
+  setStr,
+  setDex,
+  setCon,
+  setInt,
+  setWis,
+  setCha,
+  addSave,
+  removeSave,
+} from '../redux'
 import {
   abilityModifier,
   modifierString,
@@ -9,7 +18,18 @@ import {
 } from './utils'
 
 import './AbilityScore.scss'
-const AbilityScore = ({ abilityScore, ability, editAbilityScore, save }) => {
+const AbilityScore = ({
+  abilityScore,
+  ability,
+  editAbilityScore,
+  addSaveProf,
+  removeSaveProf,
+  prof,
+  saveProf,
+}) => {
+  const save = modifierString(
+    abilityModifier(abilityScore) + (saveProf ? prof : 0)
+  )
   console.log(ability, 'save', save)
   const handleChange = ({ target: { value } }) => {
     editAbilityScore(value)
@@ -26,36 +46,59 @@ const AbilityScore = ({ abilityScore, ability, editAbilityScore, save }) => {
       <div className="ability-mod">
         Mod: {abilityModifierString(abilityScore)}
       </div>
-      <div className="ability-save">Save: {save}</div>
+      <div className="ability-save">
+        <input
+          name="saveProf"
+          type="checkbox"
+          onChange={saveProf ? removeSaveProf : addSaveProf}
+          checked={saveProf}
+        />
+        Save: {save}
+      </div>
     </div>
   )
 }
 
-const mapState = ({ abilities, combatBasics: { prof } }, ownProps) => {
-  const ability = ownProps.ability
-  const abilityScore = abilities[ownProps.ability]
-  const save = modifierString(
-    abilityModifier(abilityScore) +
-      (abilities.saves.includes(ability) ? prof : 0)
-  )
-  return {
-    ability,
-    abilityScore,
-    save,
-  }
-}
+const mapState = ({ abilities, combatBasics: { prof } }, { ability }) => ({
+  ability,
+  abilityScore: abilities[ability],
+  prof,
+  saveProf: abilities.saves.includes(ability),
+})
 
 const mapDispatch = (dispatch, ownProps) => {
   return {
-    editAbilityScore: {
-      str: str => dispatch(setStr(str)),
-      dex: dex => dispatch(setDex(dex)),
-      con: con => dispatch(setCon(con)),
-      int: int => dispatch(setInt(int)),
-      wis: wis => dispatch(setWis(wis)),
-      cha: cha => dispatch(setCha(cha)),
-    }[ownProps.ability],
-  }
+    str: {
+      editAbilityScore: str => dispatch(setStr(str)),
+      addSaveProf: () => dispatch(addSave('str')),
+      removeSaveProf: () => dispatch(removeSave('str')),
+    },
+    dex: {
+      editAbilityScore: dex => dispatch(setDex(dex)),
+      addSaveProf: () => dispatch(addSave('dex')),
+      removeSaveProf: () => dispatch(removeSave('dex')),
+    },
+    con: {
+      editAbilityScore: con => dispatch(setCon(con)),
+      addSaveProf: () => dispatch(addSave('con')),
+      removeSaveProf: () => dispatch(removeSave('con')),
+    },
+    int: {
+      editAbilityScore: int => dispatch(setInt(int)),
+      addSaveProf: () => dispatch(addSave('int')),
+      removeSaveProf: () => dispatch(removeSave('int')),
+    },
+    wis: {
+      editAbilityScore: wis => dispatch(setWis(wis)),
+      addSaveProf: () => dispatch(addSave('wis')),
+      removeSaveProf: () => dispatch(removeSave('wis')),
+    },
+    cha: {
+      editAbilityScore: cha => dispatch(setCha(cha)),
+      addSaveProf: () => dispatch(addSave('cha')),
+      removeSaveProf: () => dispatch(removeSave('cha')),
+    },
+  }[ownProps.ability]
 }
 
 export default connect(
