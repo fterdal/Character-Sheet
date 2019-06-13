871c413aa985f3796bde79736779b0e23f8956ff
Finn <finnterdal@gmail.com>
2019-06-13 18:09:43 -0500

    simplified ability redux store

--- client/components/AbilityScore.js
+++ client/components/AbilityScore.js
@@ -1,15 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import {
-  setStr,
-  setDex,
-  setCon,
-  setInt,
-  setWis,
-  setCha,
-  addSave,
-  removeSave,
-} from '../redux'
+import { setAbility, addSave, removeSave } from '../redux'
 import {
   abilityModifier,
   modifierString,
@@ -30,7 +21,6 @@ const AbilityScore = ({
   const save = modifierString(
     abilityModifier(abilityScore) + (saveProf ? prof : 0)
   )
-  console.log(ability, 'save', save)
   const handleChange = ({ target: { value } }) => {
     editAbilityScore(value)
   }
@@ -66,40 +56,11 @@ const mapState = ({ abilities, combatBasics: { prof } }, { ability }) => ({
   saveProf: abilities.saves.includes(ability),
 })
 
-const mapDispatch = (dispatch, ownProps) => {
-  return {
-    str: {
-      editAbilityScore: str => dispatch(setStr(str)),
-      addSaveProf: () => dispatch(addSave('str')),
-      removeSaveProf: () => dispatch(removeSave('str')),
-    },
-    dex: {
-      editAbilityScore: dex => dispatch(setDex(dex)),
-      addSaveProf: () => dispatch(addSave('dex')),
-      removeSaveProf: () => dispatch(removeSave('dex')),
-    },
-    con: {
-      editAbilityScore: con => dispatch(setCon(con)),
-      addSaveProf: () => dispatch(addSave('con')),
-      removeSaveProf: () => dispatch(removeSave('con')),
-    },
-    int: {
-      editAbilityScore: int => dispatch(setInt(int)),
-      addSaveProf: () => dispatch(addSave('int')),
-      removeSaveProf: () => dispatch(removeSave('int')),
-    },
-    wis: {
-      editAbilityScore: wis => dispatch(setWis(wis)),
-      addSaveProf: () => dispatch(addSave('wis')),
-      removeSaveProf: () => dispatch(removeSave('wis')),
-    },
-    cha: {
-      editAbilityScore: cha => dispatch(setCha(cha)),
-      addSaveProf: () => dispatch(addSave('cha')),
-      removeSaveProf: () => dispatch(removeSave('cha')),
-    },
-  }[ownProps.ability]
-}
+const mapDispatch = (dispatch, { ability }) => ({
+  editAbilityScore: score => dispatch(setAbility(ability, score)),
+  addSaveProf: () => dispatch(addSave(ability)),
+  removeSaveProf: () => dispatch(removeSave(ability)),
+})
 
 export default connect(
   mapState,
--- client/redux/abilities.js
+++ client/redux/abilities.js
@@ -1,21 +1,16 @@
 // ACTION TYPES
-const SET_STR = 'SET_STR'
-const SET_DEX = 'SET_DEX'
-const SET_CON = 'SET_CON'
-const SET_INT = 'SET_INT'
-const SET_WIS = 'SET_WIS'
-const SET_CHA = 'SET_CHA'
+const SET_ABILITY = 'SET_ABILITY'
+
 const SET_SAVES = 'SET_SAVES'
 const ADD_SAVE = 'SET_SAVE'
 const REMOVE_SAVE = 'REMOVE_SAVE'
 
 // ACTION CREATORS
-export const setStr = str => ({ type: SET_STR, str })
-export const setDex = dex => ({ type: SET_DEX, dex })
-export const setCon = con => ({ type: SET_CON, con })
-export const setInt = int => ({ type: SET_INT, int })
-export const setWis = wis => ({ type: SET_WIS, wis })
-export const setCha = cha => ({ type: SET_CHA, cha })
+export const setAbility = (ability, score) => ({
+  type: SET_ABILITY,
+  ability,
+  score,
+})
 export const setSaves = saves => ({ type: SET_SAVES, saves })
 export const addSave = save => ({ type: ADD_SAVE, save })
 export const removeSave = save => ({ type: REMOVE_SAVE, save })
@@ -27,16 +22,16 @@ export const defaultStateAbilities = {
   int: 10,
   wis: 10,
   cha: 10,
-  saves: ['dex', 'int'],
+  saves: [],
 }
 
+const abilities = ['str', 'dex', 'con', 'int', 'wis', 'cha']
+
 const dispatchers = {
-  [SET_STR]: (state, { str }) => ({ ...state, str }),
-  [SET_DEX]: (state, { dex }) => ({ ...state, dex }),
-  [SET_CON]: (state, { con }) => ({ ...state, con }),
-  [SET_INT]: (state, { int }) => ({ ...state, int }),
-  [SET_WIS]: (state, { wis }) => ({ ...state, wis }),
-  [SET_CHA]: (state, { cha }) => ({ ...state, cha }),
+  [SET_ABILITY]: (state, { ability, score }) => {
+    if (!abilities.includes(ability)) return state
+    return { ...state, [ability]: score }
+  },
   [SET_SAVES]: (state, { saves }) => ({ ...state, saves }),
   [ADD_SAVE]: (state, { save }) => {
     if (state.saves.includes(save)) return state
