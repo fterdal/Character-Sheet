a29298dd8179455318231add90d9695863bd68d0
Finn <finnterdal@gmail.com>
2019-06-14 14:21:59 -0500

    fix styles on mobile devies

--- client/components/Abilities.js
+++ client/components/Abilities.js
@@ -3,13 +3,15 @@ import AbilityScore from './AbilityScore'
 
 import './Abilities.scss'
 const Abilities = () => (
-  <div className="abilities">
-    <AbilityScore ability="str" />
-    <AbilityScore ability="dex" />
-    <AbilityScore ability="con" />
-    <AbilityScore ability="int" />
-    <AbilityScore ability="wis" />
-    <AbilityScore ability="cha" />
+  <div className="abilities-container">
+    <div className="abilities">
+      <AbilityScore ability="str" />
+      <AbilityScore ability="dex" />
+      <AbilityScore ability="con" />
+      <AbilityScore ability="int" />
+      <AbilityScore ability="wis" />
+      <AbilityScore ability="cha" />
+    </div>
   </div>
 )
 
