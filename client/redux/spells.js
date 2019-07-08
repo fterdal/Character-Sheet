02a864863d9b961a0d71bad883c5bb3382b923e8
Finn <finnterdal@gmail.com>
2019-07-07 22:32:37 -0400

    initial state for spells

--- client/redux/spells.js
+++ client/redux/spells.js
@@ -2,11 +2,33 @@
 const SET_SPELLS = 'SET_SPELLS'
 
 // ACTION CREATORS
-export const setSpells = (spells) => ({ type: SET_SPELLS, spells })
+export const setSpells = spells => ({ type: SET_SPELLS, spells })
 
-export const defaultStateSpells = {
-  spells: [],
+/* Spells look like this:
+{
+  name: 'Magic Missile',
+  lvl: 1,
+  cast: false,
 }
+*/
+
+export const defaultStateSpells = [
+  {
+    name: 'Light',
+    lvl: 0,
+    cast: false,
+  },
+  {
+    name: 'Magic Missile',
+    lvl: 1,
+    cast: true,
+  },
+  {
+    name: 'Fireball',
+    lvl: 3,
+    cast: false,
+  },
+]
 
 const dispatchers = {
   [SET_SPELLS]: (state, { spells }) => ({ ...state, spells }),
